45e94df2e6c8975937b90c1424dca668ab81a3a1
STLK <stalkach86@gmail.com>
2016-05-09 16:52:52 +0500

    fixd

--- server.py
+++ server.py
@@ -21,11 +21,12 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 2222))
 s.listen(10)
 while True:
-    conn, addr = s.accept()
-    while True:
-        data = conn.recv(1024)
-        if data == b'close' or not data:
-            conn.close()
-            break
-        conn.send(data)
-    conn.close()
+    for i in range(10):
+        conn, addr = s.accept()
+        while True:
+            data = conn.recv(1024)
+            if data == b'close' or not data:
+                conn.close()
+                break
+            conn.send(data)
+        conn.close()
